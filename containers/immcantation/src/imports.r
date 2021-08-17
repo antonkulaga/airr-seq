5ebfc8abd29ae4ef9145b4972fbe69330901309f
Eugen Ursu <eugen@localhost.localdomain>
2021-08-17 12:37:23 +0000

    Merged clones and diversity scripts

--- containers/immcantation/src/imports.r
+++
@@ -1,9 +0,0 @@
-include <- function(pkg) {
-  if (!suppressMessages(require(pkg, character.only = TRUE)))
-    install.packages(pkg, character.only = TRUE)
-  suppressMessages(library(pkg, pkg, character.only = TRUE))
-}
-include("docopt")
-include("stringr")
-include("alakazam")
-include("dplyr")
