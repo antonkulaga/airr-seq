9aee08814d415888057abd1e6e0f95cfcd53ff45
Eugen Ursu <eugen@localhost.localdomain>
2021-09-02 12:16:42 +0300

    Clonal analysis update

--- containers/immcantation/debug.sh
+++ containers/immcantation/debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run  -v /data:/data -v `pwd`/src:/home/magus/notebooks/src --network=host -i -t immcantation_local jupyter lab --ip=0.0.0.0 .
+docker run  --memory=32gb -v /data:/data -v `pwd`/src:/home/magus/notebooks/src --network=host -i -t immcantation_local jupyter lab --ip=0.0.0.0 .
 #docker run --network=host -i -t immcantation/lab:devel
